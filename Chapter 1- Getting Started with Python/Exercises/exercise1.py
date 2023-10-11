7a700525d9b07224e8c569cbb684f8fdc1eb4373
ruwaizzzz <144207342+ruwaizzzz@users.noreply.github.com>
2023-10-11 10:28:30 +0400

    Exercises

--- Chapter 1- Getting Started with Python/Exercises/exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise1.py
@@ -1,9 +1,9 @@
-print('''
+print("""
 Twinkle, twinkle, little star,
 	How I wonder what you are! 
 		Up above the world so high,   		
 		Like a diamond in the sky. 
 Twinkle, twinkle, little star, 
-	How I wonder what you are''')
+	How I wonder what you are""")
 
 
